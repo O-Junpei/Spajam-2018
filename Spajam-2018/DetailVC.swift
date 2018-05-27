f94a8b4e7b49c79fffcb7586ba745acb8e35cbd8
junpei ono <snorlax.chemist.and.jazz@gmail.com>
2018-05-27 11:14:25 +0900

    通知前まで実装

--- Spajam-2018/DetailVC.swift
+++ Spajam-2018/DetailVC.swift
@@ -1,4 +1,7 @@
 import UIKit
+import Alamofire
+import SwiftyJSON
+
 
 class DetailVC: UIViewController {
     
@@ -27,6 +30,10 @@ class DetailVC: UIViewController {
     private var  saveBtn:UIButton!
     
     private var proteinCount:UILabel!
+    private var proteinAmount = 0
+    
+    var timer: Timer!
+
     
     
 
@@ -66,7 +73,7 @@ class DetailVC: UIViewController {
         let term = UILabel()
         term.text = "期間"
         term.textColor = UIColor.init(named: "MainGray")
-        term.frame = CGRect(x: 32, y: statusBarHeight + navigationBarHeight + 88, width: viewWidth, height: 20)
+        term.frame = CGRect(x: 32, y: statusBarHeight + navigationBarHeight + 80, width: viewWidth, height: 20)
         term.font = UIFont.boldSystemFont(ofSize: 20)
         term.textAlignment = NSTextAlignment.left
         self.view.addSubview(term)
@@ -74,7 +81,7 @@ class DetailVC: UIViewController {
         let termLabel = UILabel()
         termLabel.text = dateStr
         termLabel.textColor = UIColor.init(named: "MainGray")
-        termLabel.frame = CGRect(x: 32, y: statusBarHeight + navigationBarHeight + 120, width: viewWidth - 64, height: 16)
+        termLabel.frame = CGRect(x: 32, y: statusBarHeight + navigationBarHeight + 112, width: viewWidth - 64, height: 16)
         termLabel.font = UIFont.boldSystemFont(ofSize: 16)
         termLabel.textAlignment = NSTextAlignment.left
         self.view.addSubview(termLabel)
@@ -82,7 +89,7 @@ class DetailVC: UIViewController {
         let menu = UILabel()
         menu.text = "メニュー"
         menu.textColor = UIColor.init(named: "MainGray")
-        menu.frame = CGRect(x: 32, y: statusBarHeight + navigationBarHeight + 156, width: viewWidth, height: 20)
+        menu.frame = CGRect(x: 32, y: statusBarHeight + navigationBarHeight + 144, width: viewWidth, height: 20)
         menu.font = UIFont.boldSystemFont(ofSize: 20)
         menu.textAlignment = NSTextAlignment.left
         self.view.addSubview(menu)
@@ -91,7 +98,7 @@ class DetailVC: UIViewController {
         let menuNameLabel = UILabel()
         menuNameLabel.text = menuTitleStr
         menuNameLabel.textColor = UIColor.init(named: "MainGray")
-        menuNameLabel.frame = CGRect(x: 32, y: statusBarHeight + navigationBarHeight + 188, width: (viewWidth - 64)/2, height: 16)
+        menuNameLabel.frame = CGRect(x: 32, y: statusBarHeight + navigationBarHeight + 176, width: (viewWidth - 64)/2, height: 16)
         menuNameLabel.font = UIFont.boldSystemFont(ofSize: 16)
         menuNameLabel.textAlignment = NSTextAlignment.left
         self.view.addSubview(menuNameLabel)
@@ -100,7 +107,7 @@ class DetailVC: UIViewController {
         let menuDescriptionLabel = UILabel()
         menuDescriptionLabel.text = menuDetailStr
         menuDescriptionLabel.textColor = UIColor.init(named: "MainGray")
-        menuDescriptionLabel.frame = CGRect(x: viewWidth/2, y: statusBarHeight + navigationBarHeight + 188, width: (viewWidth - 64)/2, height: 16)
+        menuDescriptionLabel.frame = CGRect(x: viewWidth/2, y: statusBarHeight + navigationBarHeight + 176, width: (viewWidth - 64)/2, height: 16)
         menuDescriptionLabel.font = UIFont.boldSystemFont(ofSize: 16)
         menuDescriptionLabel.textAlignment = NSTextAlignment.right
         self.view.addSubview(menuDescriptionLabel)
@@ -109,15 +116,15 @@ class DetailVC: UIViewController {
         let protein = UILabel()
         protein.text = "プロテイン"
         protein.textColor = UIColor.init(named: "MainGray")
-        protein.frame = CGRect(x: 32, y: statusBarHeight + navigationBarHeight + 224, width: (viewWidth-64)-100, height: 20)
+        protein.frame = CGRect(x: 32, y: statusBarHeight + navigationBarHeight + 204, width: (viewWidth-64)-100, height: 20)
         protein.font = UIFont.boldSystemFont(ofSize: 20)
         protein.textAlignment = NSTextAlignment.left
         self.view.addSubview(protein)
         
         proteinCount = UILabel()
-        proteinCount.text = "100個"
+        proteinCount.text = String(proteinAmount) + "個"
         proteinCount.textColor = UIColor.init(named: "MainGray")
-        proteinCount.frame = CGRect(x: (viewWidth-32)-100 , y: statusBarHeight + navigationBarHeight + 224, width: 100, height: 20)
+        proteinCount.frame = CGRect(x: (viewWidth-32)-100 , y: statusBarHeight + navigationBarHeight + 204, width: 100, height: 20)
         proteinCount.font = UIFont.boldSystemFont(ofSize: 20)
         proteinCount.textAlignment = NSTextAlignment.right
         self.view.addSubview(proteinCount)
@@ -129,8 +136,8 @@ class DetailVC: UIViewController {
         let baseStart:CGFloat = 0 // 開始の角度
         let baseEnd :CGFloat = pi * 2.0// 終了の角度
         let basePath: UIBezierPath = UIBezierPath();
-        basePath.move(to: CGPoint(x:viewWidth/2, y:statusBarHeight + navigationBarHeight + 380))
-        basePath.addArc(withCenter: CGPoint(x:viewWidth/2, y:statusBarHeight + navigationBarHeight + 380), radius: 100, startAngle: baseStart, endAngle: baseEnd, clockwise: true)
+        basePath.move(to: CGPoint(x:viewWidth/2, y:statusBarHeight + navigationBarHeight + 328))
+        basePath.addArc(withCenter: CGPoint(x:viewWidth/2, y:statusBarHeight + navigationBarHeight + 328), radius: 100, startAngle: baseStart, endAngle: baseEnd, clockwise: true)
         let baseLayer = CAShapeLayer()
         baseLayer.fillColor = UIColor.init(named: "ProgressGray")?.cgColor
         baseLayer.path = basePath.cgPath
@@ -140,8 +147,8 @@ class DetailVC: UIViewController {
         let pinkStart:CGFloat = pi * 1.5 // 開始の角度
         let pinkEnd :CGFloat = pi * (0.02 * CGFloat(achievement) + 1.5) // 終了の角度
         let pinkPath: UIBezierPath = UIBezierPath();
-        pinkPath.move(to: CGPoint(x:viewWidth/2, y:statusBarHeight + navigationBarHeight + 380))
-        pinkPath.addArc(withCenter: CGPoint(x:viewWidth/2, y:statusBarHeight + navigationBarHeight + 380), radius: 100, startAngle: pinkStart, endAngle: pinkEnd, clockwise: true)
+        pinkPath.move(to: CGPoint(x:viewWidth/2, y:statusBarHeight + navigationBarHeight + 328))
+        pinkPath.addArc(withCenter: CGPoint(x:viewWidth/2, y:statusBarHeight + navigationBarHeight + 328), radius: 100, startAngle: pinkStart, endAngle: pinkEnd, clockwise: true)
         let pinkLayer = CAShapeLayer()
         pinkLayer.fillColor = UIColor.init(named: "MainPink")?.cgColor
         pinkLayer.path = pinkPath.cgPath
@@ -152,8 +159,8 @@ class DetailVC: UIViewController {
         let whiteStart:CGFloat = 0 // 開始の角度
         let whiteEnd :CGFloat = pi * 2.0// 終了の角度
         let whitePath: UIBezierPath = UIBezierPath();
-        whitePath.move(to: CGPoint(x:viewWidth/2, y:statusBarHeight + navigationBarHeight + 380))
-        whitePath.addArc(withCenter: CGPoint(x:viewWidth/2, y:statusBarHeight + navigationBarHeight + 380), radius: 80, startAngle: whiteStart, endAngle: whiteEnd, clockwise: true)
+        whitePath.move(to: CGPoint(x:viewWidth/2, y:statusBarHeight + navigationBarHeight + 328))
+        whitePath.addArc(withCenter: CGPoint(x:viewWidth/2, y:statusBarHeight + navigationBarHeight + 328), radius: 80, startAngle: whiteStart, endAngle: whiteEnd, clockwise: true)
         let whiteLayer = CAShapeLayer()
         whiteLayer.fillColor = UIColor.init(named: "BGGray")?.cgColor
         whiteLayer.path = whitePath.cgPath
@@ -161,13 +168,57 @@ class DetailVC: UIViewController {
         
         let percentLabel = UILabel()
         percentLabel.text = String(achievement) + " %"
-        percentLabel.frame = CGRect(x: 0 , y: statusBarHeight + navigationBarHeight + 360, width: viewWidth, height: 40)
+        percentLabel.frame = CGRect(x: 0 , y: statusBarHeight + navigationBarHeight + 308, width: viewWidth, height: 40)
         percentLabel.textAlignment = NSTextAlignment.center
         percentLabel.font = UIFont.boldSystemFont(ofSize: 40)
         self.view.addSubview(percentLabel)
         
         
+        let proteinBtn = UIButton()
+        proteinBtn.frame = CGRect(x: viewWidth - 72, y: statusBarHeight + navigationBarHeight + 380, width: 60, height: 60)
+        proteinBtn.layer.masksToBounds = true
+        proteinBtn.backgroundColor = UIColor.init(named: "MainPink")
+        proteinBtn.setBackgroundImage(UIImage(named: "protein"), for: UIControlState.normal)
+        proteinBtn.layer.cornerRadius = 30.0
+        proteinBtn.addTarget(self, action: #selector(proteinButtonClicked(sender:)), for:.touchUpInside)
+        self.view.addSubview(proteinBtn)
+        
+        
+        timer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
+        timer.fire()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        timer.invalidate()
     }
 
+    //basicボタンが押されたら呼ばれます
+    @objc internal func proteinButtonClicked(sender: UIButton){
+        print("basicButtonBtnClicked")
+    }
+    
+    @objc func update(tm: Timer) {
+        //この関数を繰り返す、repeat this function
+        Alamofire.request("http://spajam2018-rails-server.herokuapp.com/api/v0/user/2").responseJSON{ response in
+            switch response.result {
+            case .success:
+                let json:JSON = JSON(response.result.value ?? kill)
+                if json["is_success"].boolValue {
+                    
+                    if self.proteinAmount != json["content"]["num_protein"].intValue {
+                        self.proteinAmount = json["content"]["num_protein"].intValue
+                        self.proteinCount.text = String(self.proteinAmount) + " 個"
+                    }
+
+                } else {
+                }
+                print(json)
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 
 }
